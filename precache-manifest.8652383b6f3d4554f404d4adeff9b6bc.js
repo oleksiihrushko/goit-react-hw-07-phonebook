0773da7d81e1ee9e5861994e05bfbe0164afde76
hrushkooleksii <hrushkooleksii@gmail.com>
2020-07-14 18:58:51 +0300

    Updates

---
+++ precache-manifest.8652383b6f3d4554f404d4adeff9b6bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62f4cd658805807fb5dfccf116f34da",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "14fe3e60b56887fe6baf",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.e60ff9e7.chunk.css"
+  },
+  {
+    "revision": "40e1f3ebfd6634a816a5",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.de4fbb8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.de4fbb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14fe3e60b56887fe6baf",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.c9be5d64.chunk.js"
+  },
+  {
+    "revision": "6864241df8d162bfadf3",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.ac952d60.js"
+  }
+]);
